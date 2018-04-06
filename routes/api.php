c93d055809c1ed221209f951db7ab43c3e9f31cd
Aditya Iyer <aditya_iyer@yahoo.com>
2018-04-06 07:56:13 +0530

    jg

--- routes/api.php
+++ routes/api.php
@@ -17,4 +17,4 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::get('listmessages{liveChatId, pageToken}', 'ApiController@listMessages');
+Route::get('/listmessages{liveChatId, pageToken}', 'ApiController@listMessages');
